0c5a35600423a46233664944c8aa79a2a2975314
Ammaramd22 <ammaramd22@gmail.com>
2025-06-30 10:48:07 +0530

    complete signup page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,7 +14,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/signin" element={<SignIn/>} />
-        <Route path="/signout" element={<SignUp/>} />
+        <Route path="/signup" element={<SignUp/>} />
         <Route path="/about" element={<About/>} />
         <Route path="/profile" element={<Profile/>} />
       </Routes>
