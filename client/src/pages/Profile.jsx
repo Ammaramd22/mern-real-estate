a078f32037c48f9d08ac9342971bcfe3db0ca985
Ammaramd22 <ammaramd22@gmail.com>
2025-07-04 23:49:10 +0530

    complete create listing page ui

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -21,6 +21,7 @@ import {
   signOutUserSuccess,
 } from "../redux/user/userSlice.js";
 import { useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 
 //import { get } from "mongoose";
 
@@ -196,6 +197,10 @@ export default function Profile() {
         <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95">
           {loading ? 'Loading....': 'Update' }
         </button>
+        <Link className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95" to={"/create-listing"} >
+        Create Listing
+        
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
         <span onClick={handleDeleteUser} className="text-red-700 cursor-pointer">Delete Account</span>
